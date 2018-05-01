fa9da231cd06e37bd9817d3ad30ccb0d961957a3
Hyejin Kim <khj_1225@naver.com>
2018-05-01 11:14:28 +0000

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+    def new
+    end
+    
+    def index
+       @articles = Article.all 
+    end
+    
+    def create
+        params.permit!
+        @article = Article.new(params[:article])
+        
+        @article.save
+        redirect_to @article    
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def destroy
+        article = Article.find(params[:id])
+        article.destroy
+        
+        redirect_to articles_path
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        params.permit!
+        @article = Article.find(params[:id])
+        @article.update(params[:article])
+        
+        redirect_to @article
+    
+    end
+    
+end
+
+
+
